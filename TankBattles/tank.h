788fc824dd5269fd16bff4883fd131a5171c3800
Lonsofore <www.wmsys@gmail.com>
2015-11-20 23:55:42 +1000

    Исправлено пропадание управления танком (Issue #10)

--- TankBattles/tank.h
+++ TankBattles/tank.h
@@ -6,6 +6,7 @@
 #include <QGraphicsItem>
 #include <QMediaPlayer>
 #include <QPixmap>
+#include <QMouseEvent>
 
 class Tank: public QObject, public QGraphicsPixmapItem
 {
@@ -13,6 +14,7 @@ class Tank: public QObject, public QGraphicsPixmapItem
 public:
     Tank(QGraphicsItem * parent=0);
     void keyPressEvent(QKeyEvent *event);
+
     void rotate();
     int degree;
     int speed;
